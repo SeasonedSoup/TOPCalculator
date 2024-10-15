5c3472ba0b06de0af2ad5bb5766dc33d304d981c
SeasonedSoup <emmetteagoncillo1@gmail.com>
2024-10-15 09:47:46 +0800

    Initialize three variables and Create operate function
    The three variables will be used for dom manipul
    ation in making the calculator and the operate
    function will take in these three and check what
    operator it contains then calls the relating
    operator function

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ const multiply = function(x, y) {
 const divide = function(x, y) {
     return x / y;
 }
+
+let firstNum;
+let operator;
+let secondNum;
+//operate calls the four operator function based on string
+const operate = function(operator, firstNum, secondNum) {
+    switch(op){
+        case '+':
+            return add(firstNum, secondNum);
+
+        case '-':
+            return subtract(firstNum, secondNum);
+        
+        case '*':
+            return multiply(firstNum, secondNum);
+        
+        case '/':
+            return divide(firstNum, secondNum)
+    }
+        
+}
