6b0566772ef7f5db00a3a0610bb94fb491c3aa20
SeasonedSoup <emmetteagoncillo1@gmail.com>
2024-10-15 09:22:27 +0800

    Add the rest of the operation functions
    after will now do create three variables as top
    recommends which are the number the operator and
    the number again

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let add = ((x, y) => x + y)
+const add = function(x, y) {
+    return x + y;
+};
+const subtract = function(x, y) {
+    return x - y; 
+};
+
+const multiply = function(x, y) {
+    return x * y;
+};
+
+const divide = function(x, y) {
+    return x / y;
+}
